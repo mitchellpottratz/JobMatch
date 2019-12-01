64f1aec82c4b9df2b847a27253d74accf324d551
Mitchell Pottratz <mitchellpottratz@Mitchells-MacBook-Air.local>
2019-12-01 13:48:55 -0600

    new company form renders on page

--- JobMatch/company_users/forms.py
+++ JobMatch/company_users/forms.py
@@ -1,3 +1,22 @@
+from django.forms import ModelForm
+from .models import Company
+
+
+# this form is used to create a new company
+class CompanyForm(ModelForm):
+
+	class Meta:
+		model = Company
+		fields = ['name', 'image']
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields['name'].widget.attrs.update({
+			'class': 'form-control'
+		})
+		# self.fields['image'].widget.attrs.update({
+			
+		# })
 
 
 
--- JobMatch/company_users/views.py
+++ JobMatch/company_users/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from users.models import User
 from .models import Company
+from .forms import CompanyForm
 
 
 # this view is where registered company users can join 
@@ -14,7 +15,23 @@ def join(request):
 # this view is where registered company users can create 
 # a new company
 def create_company(request):
-	pass
+
+	# if the form was submitted
+	if request.method == 'POST':
+
+		# creates a new form instance passing in the data
+		form = CompanyForm(request.POST)
+
+		# if the form is valid
+		if form.is_valid():
+			pass
+
+	# if the https method is GET
+	else:
+		# creates a form instance with no data
+		form = CompanyForm() 
+		
+	return render(request, 'create_company.html', {'form': form})
 
 
 
