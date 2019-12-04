78e3944c05b2982addda10b0e00ddd165c339d63
Mitchell Pottratz <mitchellpottratz@Mitchells-MacBook-Air.local>
2019-12-04 09:23:21 -0600

    company_account renders index page

--- JobMatch/company_account/urls.py
+++ JobMatch/company_account/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
 	
+	# where company users can view all of their information
+	path('index/', views.index, name='index'),
+
+
 ]
 
 
--- JobMatch/company_account/views.py
+++ JobMatch/company_account/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+# this view is where a company user can view all
+# of their information
+@login_required
+def index(request):
+	return render(request, 'index.html')
+
+
