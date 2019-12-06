95a42c165fd3d26c348cc9e559e8dc0c6156d3eb
Mitchell Pottratz <mitchellpottratz@Mitchells-MacBook-Air.local>
2019-12-06 11:43:59 -0600

    candidate user can create a project

--- JobMatch/projects/forms.py
+++ JobMatch/projects/forms.py
@@ -6,6 +6,21 @@ from .models import Project
 # form for creating a new project
 class ProjectForm(forms.ModelForm):
 
+	start_date = forms.DateTimeField(
+		input_formats=['%d/%m/%Y'],
+		widget=forms.DateTimeInput(attrs={
+			'class': 'form-control datetimepicker-input',
+			'data-target': '#datetimepicker1'
+		})
+	)
+	end_date = forms.DateTimeField(
+		input_formats=['%d/%m/%Y'],
+		widget=forms.DateTimeInput(attrs={
+			'class': 'form-control datetimepicker-input',
+			'data-target': '#datetimepicker1'
+		})
+	)
+
 	class Meta:
 		model = Project
 		exclude = ['user', 'last_updated', 'timestamp']
@@ -13,9 +28,13 @@ class ProjectForm(forms.ModelForm):
 
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		
+
 		# adds the class form-control to every field 
 		for field in self.fields:
 			self.fields[field].widget.attrs.update({
 				'class': 'form-control'
 			})
+
+		self.fields['start_date'].widget.attrs.update({
+			'type': 'date'
+		})
--- JobMatch/projects/views.py
+++ JobMatch/projects/views.py
@@ -14,13 +14,25 @@ def new(request):
 
 	# if the form was submitted
 	if request.method == 'POST':
-		form = ProjectForm(request.POST)
+		form = ProjectForm(request.POST, request.FILES)
+
+		# if the form is valid 
+		if form.is_valid():
+			project = form.save(commit=False)
+			project.user = request.user
+			project.save()
+			return redirect('/candidate-account/')
+
+		# if the form is not valid
+		else:
+			print('form invalid')
+			print(form.errors)
 
 	# if the form hasnt been submitted
 	else:
 		form = ProjectForm()
 
-	return render(request, 'projects/new.html')
+	return render(request, 'projects/new.html', {'form': form})
 
 
 	
