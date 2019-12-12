4db57fc728d6ceb28a5a6db766162a0635536b90
Mitchell Pottratz <mitchellpottratz@Mitchells-MacBook-Air.local>
2019-12-11 23:28:09 -0600

    changed to debug

--- JobMatch/settings.py
+++ JobMatch/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
