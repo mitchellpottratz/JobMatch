9ee9568c90e42d1d287eba03bb0142df36b60f47
Mitchell Pottratz <mitchellpottratz@Mitchells-MacBook-Air.local>
2019-12-11 20:22:05 -0600

    styled company user account page

--- company_account/views.py
+++ company_account/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.decorators import login_required
 # import decorator that checks if current user is a company user
 from .decorators import company_account_required
 
+# models imports
+from job_posts.models import JobPost
+
 
 # this view is where a company user can view all
 # of their information
@@ -19,6 +22,9 @@ def index(request):
 	# gets all of the companies users excluding the current user
 	company_users = company.users.all().exclude(id=user.id)
 
+	# gets all the companies job posts
+	job_posts = JobPost.objects.filter(company_account=company).order_by('-timestamp')
+
 	# if the current user is the admin
 	if company.admin == user:
 		is_admin = True
@@ -31,6 +37,7 @@ def index(request):
 		'company': company,
 		'company_users': company_users, 
 		'is_admin': is_admin,
+		'job_posts': job_posts,
 		'nav': 'account'
 	}
 	return render(request, 'company_account/index.html', context)
